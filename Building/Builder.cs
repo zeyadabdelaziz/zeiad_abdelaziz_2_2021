fbbd978ecf49365c9dde5c03a347c4d198ed0951
zeiad <Thoshiba@DESKTOP-8SDJ1A2>
2021-09-19 05:23:22 -0700

    part 3

---
+++ Building/Builder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Building
+{
+    public class Builder
+    {
+        public void Build()
+        {
+            Console.WriteLine("Building...");
+        }
+    }
+}
