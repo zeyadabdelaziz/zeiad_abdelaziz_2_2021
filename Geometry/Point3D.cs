acbfde892276bbc79e51f7f4cf059299fe8a0e37
zeiad <Thoshiba@DESKTOP-8SDJ1A2>
2021-09-19 05:15:41 -0700

    part 2

---
+++ Geometry/Point3D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    class Point3D
+    {
+        private double X;
+        private double Y;
+        private double Z;
+
+        public Point3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
