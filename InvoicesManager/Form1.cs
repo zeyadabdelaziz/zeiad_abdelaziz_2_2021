642c9f0287cdf2854accbd624166b77e72cf4237
Thoshiba <Thoshiba@DESKTOP-8SDJ1A2>
2021-09-19 05:05:46 -0700

    Add project files.

---
+++ InvoicesManager/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoicesManager
+{
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var res = expenses()
+                .Select(x => x.Price + ";" + x.Date.ToString("yyyy/MM/dd") + ";" + x.Exptype);
+
+            textBox2.Text = string.Join("\r\n", res);
+        }
+
+        public List<exp> expenses()
+        {
+            string path = textBox1.Text;
+
+            if (System.IO.File.Exists(path) == false)
+            {
+                MessageBox.Show("not exist");
+                return null;
+            }
+
+            var allfile = System.IO.File.ReadAllLines(path).Skip(1);
+
+            List<exp> exps = new List<exp>();
+            foreach (var l in allfile)
+            {
+                var split = l.Split(';');
+                var price = Convert.ToDouble(split[0].Replace(".", System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator));
+                var date = DateTime.ParseExact(split[1], "yyyy-MM-dd", null);
+                var type = split[2];
+
+
+
+                exp expense = new exp(price, date, type);
+                exps.Add(expense);
+            }
+            return exps;
+        }
+
+
+    }
+
+   
+}
---
+++ InvoicesManager/exp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoicesManager
+{
+    public class exp
+    {
+        public double Price;
+        public DateTime Date;
+        public string Exptype;
+
+        public exp(double pr, DateTime d, string etype)
+        {
+            Date = d;
+            Price = pr;
+            Exptype = etype;
+        }
+    }
+}
