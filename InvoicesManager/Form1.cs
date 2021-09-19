3b22bcc749a0562d19bf6a9312fb860652a66db4
zeiad <Thoshiba@DESKTOP-8SDJ1A2>
2021-09-19 05:32:00 -0700

    task 1/2

--- InvoicesManager/Form1.cs
+++ InvoicesManager/Form1.cs
@@ -24,6 +24,10 @@ namespace InvoicesManager
                 .Select(x => x.Price + ";" + x.Date.ToString("yyyy/MM/dd") + ";" + x.Exptype);
 
             textBox2.Text = string.Join("\r\n", res);
+
+            var res1 = expenses();
+            var totalexps = res1.Sum(x => x.Price);
+            textBox2.Text += "total expense " + totalexps;
         }
 
         public List<exp> expenses()
